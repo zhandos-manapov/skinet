821b52544dff382616a2f1be939ec147a7fd63a5
zhandos-manapov <zandosmanapov@gmail.com>
2023-10-05 11:27:23 +0600

    added product branch and type entities and repo

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,27 +14,38 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
-    private readonly StoreContext _storeContext;
+    private readonly IProductRepository _productRepository;
 
-    public ProductsController(StoreContext storeContext)
+    public ProductsController(IProductRepository productRepository)
     {
-      this._storeContext = storeContext;
+      this._productRepository = productRepository;
     }
-
-
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var product = await this._storeContext.Products.ToListAsync();
-      return product;
+      var product = await this._productRepository.GetProductsAsync();
+      return Ok(product);
     }
 
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return await this._storeContext.Products.FindAsync();
+      return await this._productRepository.GetProductByIdAsync(id);
     }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+    {
+      return Ok(await this._productRepository.GetProductBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+      return Ok(await this._productRepository.GetProductTypesAsync());
+    }
+
   }
 }
